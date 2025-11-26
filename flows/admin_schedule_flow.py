c39298aad33b2b28bb1ff8dbaaf8ee18961a81e7
DaniilBolshoj <158136050+DaniilBolshoj@users.noreply.github.com>
2025-11-26 09:05:26 +0000

    Added code to admin_schedule_flow.py

--- flows/admin_schedule_flow.py
+++ flows/admin_schedule_flow.py
@@ -1,1 +1,14 @@
 # Здесь будут callback-хендлеры для start_time, end_time, slot_duration
+from aiogram import Router, F
+from aiogram import types
+router = Router()
+
+@router.message(F.text == "🕒 Настроить время работы")
+async def set_working_hours(msg: types.Message):
+    await msg.answer("Настройка времени работы... (пока заглушка)")
+
+@router.message(F.text == "⬅️ Назад в меню")
+async def back_to_admin_menu(msg: types.Message):
+    from keyboards.admin_keyboard import admin_menu_kb
+    await msg.answer("Возврат в админ-меню", reply_markup=admin_menu_kb())
+

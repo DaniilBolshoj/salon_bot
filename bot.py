d8f99458c0d25f4e82d1d011fefda84e05dcd51a
DaniilBolshoj <158136050+DaniilBolshoj@users.noreply.github.com>
2025-11-26 10:32:44 +0000

    Updated files or chenged some lines

--- bot.py
+++ bot.py
@@ -13,9 +13,9 @@ from handlers.users.booking import router as booking_router
 from handlers.users.contacts import router as contacts_router
 
 from handlers.admin.admin_menu import router as admin_menu_router
-from handlers.admin.masters import router as admin_masters_router
+#from handlers.admin.masters import router as admin_masters_router
 from handlers.admin.schedule import router as admin_schedule_router
-from handlers.admin.services import router as admin_services_router
+#from handlers.admin.services import router as admin_services_router
 
 from flows.universal_router import router as universal_router
 
@@ -27,7 +27,6 @@ bot = Bot(
 )
 
 dp = Dispatcher()
-
 
 async def on_startup():
     print("🔄 Инициализация базы данных...")
--- utils/config_loader.py
+++ utils/config_loader.py
@@ -3,7 +3,20 @@ from dotenv import load_dotenv
 
 load_dotenv(dotenv_path="c:/Users/Daniil/Desktop/salon_bot/api.env")
 
+# Попробуем сначала загрузить из указанного пути, если не получится — из ./api.env
+if os.path.exists("c:/Users/Daniil/Desktop/salon_bot/api.env"):
+    load_dotenv("c:/Users/Daniil/Desktop/salon_bot/api.env")
+    print("✅ .env файл загружен успешно.")
+elif os.path.exists("./api.env"):
+    load_dotenv("./api.env")
+    print("⚠️ .env файл не найден по указанному пути, загружен ./api.env")
+else:
+    print("❌ .env файл не найден.")
+
 BOT_TOKEN = os.getenv("TOKEN")
-OWNER_ID = int(os.getenv("ADMIN_ID"))
+admin_id = os.getenv("ADMIN_ID")
+if not admin_id:
+    raise ValueError("ADMIN_ID не задан в .env файле")
+OWNER_ID = int(admin_id)
 DB_PATH = os.getenv("DB_PATH")
 THRESHOLD = 10_000  # для генераторов слотов
